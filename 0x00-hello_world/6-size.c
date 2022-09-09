7503ce7e0eee0b9fecd3818dfb8b26e7dcabd295
Fluxxxis <sanyadeakindayo@yahoo.com>
2022-09-08 22:46:20 -0700

    task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,6 @@
 * Return: 0 if exited properly, non-zer otherwise
 */
 int main(void)
-
 {
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
@@ -14,5 +13,6 @@ int main(void)
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 	printf("Size of a long ilong int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
 	return (0);
 }
